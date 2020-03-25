0ad43b1072c59443b71a8f39719110aef3d11d03
Austin Kohler <austinckohler@gmail.com>
2020-03-25 20:57:15 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -14,7 +14,11 @@ end
 
 def literal_aoh
   aoh_literal = [
-    assembled_aoh.sort
+    {name: "Don Gately", occupation: "Live-in Staff"},
+    { name:  "Joelle van Dyne", occupation: "Radio Personality" },
+    { name:  "Pat Monteseian", occupation: "Staff" },
+    { name:  "Kate Gompert", occupation: "None" },
+    { name:  "Bruce Green", occupation: "Fan of Mildred" }
     ]
 end
 
